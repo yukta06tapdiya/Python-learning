7ded87c3884662367707b1b49483c4c34494d381
Yukta Tapdiya <yukta06tapdiya@gmail.com>
2026-02-01 21:47:11 +0530

    Add files via upload
    Using Arithmetic Operators

---
+++ Program3.py
@@ -0,0 +1,14 @@
+m=6
+n=3
+Add=m+n
+print("Addition of two integer is",Add)
+Sub=m-n
+print("Substraction of two integer is",Sub)
+Product=m*n
+print("Multiplication of two integer is",Product)
+Division=m/n
+print("Division of two integer is",Division)
+Exponential=m**n
+print("Exponential value of  integer is",Exponential)
+Modulus=m%n
+print("Modulus of m is to n",Modulus)
